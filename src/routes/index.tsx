f0e2609a4f51b1dd88ffa85435d58ba7e2bbb316
GeeckoSama <julienheller81@gmail.com>
2024-02-23 15:09:51 +0100

    Mise à jour des routes pour charger les photos depuis Supabase

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,40 @@
 import { component$ } from "@builder.io/qwik";
-import type { DocumentHead } from "@builder.io/qwik-city";
+import { routeLoader$, type DocumentHead } from "@builder.io/qwik-city";
+import { createServerClient } from "supabase-auth-helpers-qwik";
+import type { Database } from "~/lib/schema";
+
+export interface Photo {
+  id: number;
+  title: string;
+  description: string;
+  url: string;
+  create_at: Date;
+  updated_at: Date;
+}
+
+export const usePhotos = routeLoader$(async (requestEvent) => {
+  const supabaseClient = createServerClient<Database>(
+    requestEvent.env.get("PUBLIC_SUPABASE_URL")!,
+    requestEvent.env.get("PUBLIC_SUPABASE_ANON_KEY")!,
+    requestEvent,
+  );
+  return await supabaseClient.from("photos").select("*");
+});
 
 export default component$(() => {
+  const signal = usePhotos();
   return (
     <>
       <h1>Hello world</h1>
+      {signal.value.data?.map((photo) => (
+        <img
+          key={photo.id}
+          src={photo.url ?? ""}
+          alt={photo.description ?? ""}
+          width={500}
+          height={500}
+        />
+      ))}
     </>
   );
 });
