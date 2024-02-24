f16262c210bdf81c5e82e0102e370bf459c86e9b
GeeckoSama <julienheller81@gmail.com>
2024-02-24 17:31:44 +0100

    Mise à jour de la barre de navigation de l'administration

--- src/components/admin-navbar/admin-navbar.tsx
+++ src/components/admin-navbar/admin-navbar.tsx
@@ -10,22 +10,68 @@ export const AdminNavbar = component$(() => {
   });
 
   return (
-    <div class="fixed top-0 flex w-full flex-1 justify-between rounded-b-md bg-gray-50 p-4 text-gray-900 shadow-lg dark:bg-gray-800 dark:text-gray-100">
-      <div>
-        <h1 class="text-xl font-semibold text-gray-800 dark:text-gray-100">
-          Administration
-        </h1>
-      </div>
-      <div>
-        {user.value && (
-          <>
-            <button
-              onClick$={handleSignOut}
-              class="rounded-md border-2 border-red-400 px-2 py-1.5 font-semibold transition-all hover:bg-red-400 hover:text-gray-50"
+    <div class="navbar bg-base-100 fixed top-0 shadow-lg z-50">
+      <div class="navbar-start">
+        <div class="dropdown">
+          <div tabIndex={0} role="button" class="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
             >
-              Se déconnecter
-            </button>
-          </>
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+          >
+            <li>
+              <a href="#">Photos</a>
+            </li>
+            <li>
+              <a href="#">Vidéos</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+          </ul>
+        </div>
+        <a href="/" class="btn btn-ghost text-xl">
+          Portfolio
+        </a>
+      </div>
+      <div class="navbar-center hidden lg:flex">
+        {user.value && (
+          <ul class="menu menu-horizontal px-1">
+            <li>
+              <a href="#">Photos</a>
+            </li>
+            <li>
+              <a href="#">Vidéos</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+          </ul>
+        )}
+      </div>
+      <div class="navbar-end">
+        {user.value && (
+          <button
+            type="button"
+            class="btn btn-outline btn-error"
+            onClick$={handleSignOut}
+          >
+            Se déconnecter
+          </button>
         )}
       </div>
     </div>
--- src/components/sigin/sigin.tsx
+++ src/components/sigin/sigin.tsx
@@ -4,21 +4,21 @@ import { supabase } from "~/lib/supabse";
 
 const LoginSchema = object({
   email: string([
-    minLength(1, "Please enter your email."),
-    email("The email address is badly formatted."),
+    minLength(1, "Veuillez entrer votre adresse email."),
+    email("Veuillez entrer une adresse email valide."),
   ]),
   password: string([
-    minLength(1, "Please enter your password."),
-    minLength(6, "Your password must have 8 characters or more."),
+    minLength(1, "Veuillez entrer votre mot de passe."),
+    minLength(6, "Votre mot de passe doit contenir au moins 6 caractères."),
   ]),
 });
 
 export const Sigin = component$(() => {
   const email = useSignal("");
   const password = useSignal("");
+  const errorText = useSignal("");
 
   const handleSubmit = $(async (event: any) => {
-    console.log("Submitting form");
     try {
       event.preventDefault();
       const values = parse(LoginSchema, {
@@ -33,46 +33,123 @@ export const Sigin = component$(() => {
       if (response.error) {
         throw new Error(response.error.message);
       }
-      console.log("User is logged in");
-    } catch (error) {
+    } catch (error: any) {
       console.error("Error signing in", error);
+      errorText.value = error.message;
     }
   });
   return (
-    <div class="mx-auto max-w-2xl rounded-md bg-gray-50 p-4 shadow-md dark:bg-gray-800">
+    <div class="card bg-base-100 mx-auto max-w-xl shadow-xl">
+      <div class="card-body">
+        <h2 class="card-title mb-4">Se connecter</h2>
+        <form class="flex flex-col gap-4">
+          <label class="input input-bordered flex items-center gap-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 16 16"
+              fill="currentColor"
+              class="h-4 w-4 opacity-70"
+            >
+              <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
+              <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
+            </svg>
+            <input
+              type="email"
+              autoComplete={"email"}
+              class="grow"
+              placeholder="Email"
+              bind:value={email}
+            />
+          </label>
+
+          <label class="input input-bordered flex items-center gap-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 16 16"
+              fill="currentColor"
+              class="h-4 w-4 opacity-70"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+                clip-rule="evenodd"
+              />
+            </svg>
+            <input
+              type="password"
+              autoComplete={"current-password"}
+              class="grow"
+              placeholder="password"
+              bind:value={password}
+            />
+          </label>
+          <div class="card-actions justify-end">
+            <p class="text-red-500">{errorText}</p>
+            <button
+              type="button"
+              class="btn btn-primary"
+              disabled={email.value === "" || password.value === ""}
+              onClick$={handleSubmit}
+            >
+              Connexion
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+});
+
+/* <div class="mx-auto max-w-2xl rounded-md bg-gray-50 p-4 shadow-md dark:bg-gray-800">
       <h1 class="mb-8 text-center text-3xl font-bold">Se connecter</h1>
       <form class="flex flex-col gap-4">
-        <div>
-          <label for="email" class="text-lg font-semibold">
-            Email
-          </label>
+        <label class="input input-bordered flex items-center gap-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 16 16"
+            fill="currentColor"
+            class="h-4 w-4 opacity-70"
+          >
+            <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
+            <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
+          </svg>
           <input
             type="email"
-            id="email"
-            class="w-full rounded-md border border-gray-300 p-2"
+            autoComplete={"email"}
+            class="grow"
+            placeholder="Email"
             bind:value={email}
           />
-        </div>
-        <div>
-          <label for="password" class="text-lg font-semibold">
-            Mot de passe
-          </label>
+        </label>
+
+        <label class="input input-bordered flex items-center gap-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 16 16"
+            fill="currentColor"
+            class="h-4 w-4 opacity-70"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+              clip-rule="evenodd"
+            />
+          </svg>
           <input
             type="password"
-            id="password"
-            class="w-full rounded-md border border-gray-300 p-2"
+            autoComplete={"current-password"}
+            class="grow"
+            placeholder="password"
             bind:value={password}
           />
-        </div>
+        </label>
         <button
           type="button"
-          class="rounded-md bg-gray-700 p-2 font-semibold text-white disabled:cursor-not-allowed disabled:bg-gray-300 disabled:text-gray-700"
+          class="btn btn-primary w-full"
           disabled={email.value === "" || password.value === ""}
           onClick$={handleSubmit}
         >
           Login
         </button>
       </form>
-    </div>
-  );
-});
+    </div> */
