553a08ccefbb989b09c40a0393d9826954cbb48f
GeeckoSama <julienheller81@gmail.com>
2024-02-29 09:33:26 +0100

    Mise à jour de la configuration de Supabase pour utiliser les variables d'environnement

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,6 +2,6 @@ import { createClient } from "@supabase/supabase-js";
 import type { Database } from "./schema";
 
 export const supabase = createClient<Database>(
-  import.meta.env.PUBLIC_SUPABASE_URL,
-  import.meta.env.PUBLIC_SUPABASE_ANON_KEY
+  import.meta.env.PUBLIC_SUPABASE_URL ?? process.env['SUPABASE_URL'],
+  import.meta.env.PUBLIC_SUPABASE_ANON_KEY ?? process.env['SUPABASE_ANON_KEY']
 );
