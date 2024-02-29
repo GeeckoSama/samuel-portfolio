3a9daad37ef5c3235417910cec9013defbfc3330
GeeckoSama <julienheller81@gmail.com>
2024-02-29 09:54:43 +0100

    Mise à jour de la configuration de Supabase

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -4,13 +4,13 @@ import { RequestEvent, RequestEventAction, RequestEventLoader } from "@builder.i
 import { createServerClient } from "supabase-auth-helpers-qwik";
 
 export const supabase = createClient<Database>(
-  import.meta.env.PUBLIC_SUPABASE_URL ?? process.env['SUPABASE_URL'],
-  import.meta.env.PUBLIC_SUPABASE_ANON_KEY ?? process.env['SUPABASE_ANON_KEY']
+  process.env['SUPABASE_URL'] ?? import.meta.env.PUBLIC_SUPABASE_URL,
+  process.env['SUPABASE_ANON_KEY'] ?? import.meta.env.PUBLIC_SUPABASE_ANON_KEY
 );
 
 export const supabaseServer = (requestEvent: RequestEvent | RequestEventAction | RequestEventLoader) => {
-  const supabase_url = requestEvent.env.get("PUBLIC_SUPABASE_URL") ?? process.env['SUPABASE_URL'];
-  const supabase_anon_key = requestEvent.env.get("PUBLIC_SUPABASE_ANON_KEY") ?? process.env['SUPABASE_ANON_KEY'];
+  const supabase_url = process.env['SUPABASE_URL'] ?? requestEvent.env.get("PUBLIC_SUPABASE_URL");
+  const supabase_anon_key = process.env['SUPABASE_ANON_KEY'] ?? requestEvent.env.get("PUBLIC_SUPABASE_ANON_KEY");
   if (!supabase_url || !supabase_anon_key) {
     throw new Error("Missing supabase url or anon key");
   }
