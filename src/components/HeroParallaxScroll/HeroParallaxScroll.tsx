5507b02e9652249242e3984c80d0c879f7411142
GeeckoSama <julienheller81@gmail.com>
2024-03-01 11:17:02 +0100

    Ajouter un composant HeroParallaxScroll à la page d'accueil

---
+++ src/components/HeroParallaxScroll/HeroParallaxScroll.tsx
@@ -0,0 +1,89 @@
+import {
+  component$,
+  useSignal,
+  useVisibleTask$,
+} from "@builder.io/qwik";
+import { gsap } from "gsap";
+import styles from './HeroParallaxScroll.module.css'
+
+export interface HeroParallaxScrollProps {
+  title: string;
+  word: string;
+  images: string[];
+}
+
+export const HeroParallaxScroll = component$<HeroParallaxScrollProps>(
+  (props) => {
+    const containerRef = useSignal<Element>();
+    const titleRef = useSignal<Element>();
+    const lettersRefs = useSignal<Element[]>([]);
+    const imagesRefs = useSignal<Element[]>([]);
+
+    // eslint-disable-next-line qwik/no-use-visible-task
+    useVisibleTask$(() => {
+      const context = gsap.context(() => {
+        const tl = gsap
+          .timeline({
+            scrollTrigger: {
+              trigger: containerRef.value,
+              start: "top bottom",
+              end: "bottom top",
+              scrub: true,
+            },
+          })
+          .to(titleRef, { y: -50 }, 0)
+          .to(imagesRefs.value[1], { y: -150 }, 0)
+          .to(imagesRefs.value[2], { y: -255 }, 0);
+        lettersRefs.value.forEach((letter) => {
+          tl.to(
+            letter,
+            {
+              top: Math.floor(Math.random() * 75) + 25,
+            },
+            0,
+          );
+        });
+      });
+      return () => context.revert();
+    });
+
+    return (
+      <div ref={containerRef} class={styles.container}>
+        <div class={styles.body}>
+          <h1 ref={titleRef}>
+            {props.title}
+          </h1>
+          <div class={styles.word}>
+            <p>
+              {props.word.split("").map((letter, i) => {
+                return (
+                  <span
+                    key={`l_${i}`}
+                    ref={(el) => (lettersRefs.value[i] = el)}
+                    class="relative"
+                  >
+                    {letter}
+                  </span>
+                );
+              })}
+            </p>
+          </div>
+        </div>
+
+        <div class={styles.images}>
+          {props.images.map((image, i) => {
+            return (
+              <div
+                key={`i_${i}`}
+                ref={(el) => (imagesRefs.value[i] = el)}
+                class={styles.imageContainer}
+              >
+                <img src={image} alt="image" />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  },
+);
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,7 @@
 import { component$ } from "@builder.io/qwik";
 import { routeLoader$, type DocumentHead } from "@builder.io/qwik-city";
 import { HeroImgGrowWithScroll } from "~/components/HeroImgGrowWithScroll/HeroImgGrowWithScroll";
+import { HeroParallaxScroll } from "~/components/HeroParallaxScroll/HeroParallaxScroll";
 import { supabaseServer } from "~/lib/supabase";
 
 export interface Photo {
@@ -27,6 +28,15 @@ export default component$(() => {
         imgHeight={1080}
         imgWidth={1920}
         text="samuel freret"
+      />
+      <HeroParallaxScroll
+        title="Titre"
+        word="lorem"
+        images={[
+          "https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg",
+          "https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg",
+          "https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg",
+        ]}
       />
     </>
   );
